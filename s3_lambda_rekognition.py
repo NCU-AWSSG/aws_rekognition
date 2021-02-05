e2488369a39e0edf2ec63b496081c9556e691d77
fdj612 <fdj612@users.noreply.github.com>
2021-02-05 16:30:48 +0800

    Add files via upload

---
+++ s3_lambda_rekognition.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+*Lambda*(/)
+from __future__ import print_function
+
+import boto3
+from decimal import Decimal
+import json
+import urllib
+
+print('Loading function')
+
+rekognition = boto3.client('rekognition')
+
+
+# --------------- Helper Functions to call Rekognition APIs ------------------
+
+
+def detect_faces(bucket, key):
+    response = rekognition.detect_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    return response
+
+
+def detect_labels(bucket, key):
+    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+
+    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
+    # Note: role used for executing this Lambda function should have write access to the table.
+    #table = boto3.resource('dynamodb').Table('MyTable')
+    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
+    #table.put_item(Item={'PK': key, 'Labels': labels})
+    return response
+
+
+def index_faces(bucket, key):
+    # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
+    #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
+    response = rekognition.index_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}}, CollectionId="BLUEPRINT_COLLECTION")
+    return response
+
+
+# --------------- Main handler ------------------
+
+
+def lambda_handler(event, context):
+    '''Demonstrates S3 trigger that uses
+    Rekognition APIs to detect faces, labels and index faces in S3 Object.
+    '''
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    print(key)
+    try:
+        if (".jpg" in key) or (".png" in key):
+                    
+            # Calls rekognition DetectFaces API to detect faces in S3 object
+            # response = detect_faces(bucket, key)
+            # print(response)
+    
+            # Calls rekognition DetectLabels API to detect labels in S3 object
+            response = detect_labels(bucket, key)
+            print(response)
+    
+            # Calls rekognition IndexFaces API to detect faces in S3 object and index faces into specified collection
+            # response = index_faces(bucket, key)
+    
+            # Print response to console.
+            # print(response)
+
+        return response
+    except Exception as e:
+        print(e)
+        print("Error processing object {} from bucket {}. ".format(key, bucket) +
+              "Make sure your object and bucket exist and your bucket is in the same region as this function.")
+        raise e
+
+
+# In[ ]:
+
+
+*測試JSON*(/)
+
+{
+  "Records": [
+    {
+      "eventVersion": "2.0",
+      "eventSource": "aws:s3",
+      "awsRegion": "eu-west-1",
+      "eventTime": "1970-01-01T00:00:00.000Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "EXAMPLE"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "127.0.0.1"
+      },
+      "responseElements": {
+        "x-amz-request-id": "EXAMPLE123456789",
+        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "testConfigRule",
+        "bucket": {
+          "name": "amazon-rekognition-fdj612",
+          "ownerIdentity": {
+            "principalId": "EXAMPLE"
+          },
+          "arn": "arn:aws:s3:::amazon-rekognition-fdj612"
+        },
+        "object": {
+          "key": "laugh.jpg",
+          "size": 1024,
+          "eTag": "0123456789abcdef0123456789abcdef",
+          "sequencer": "0A1B2C3D4E5F678901"
+        }
+      }
+    }
+  ]
+}
+
